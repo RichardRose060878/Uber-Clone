57ecab9b2f738549ca065f93245417573cf6080a
Richard <richardrose060878@gmail.com>
2021-08-03 16:54:56 +0100

    Complete build

---
+++ screens/HomeScreen.js
@@ -0,0 +1,70 @@
+// rnfes to create this initially (ES7 SNIPPET)
+// SafeAreaView will move the item away from the top of the notch area
+// View is same as div
+
+import React from "react";
+import { Text, View, SafeAreaView, Image } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setOrigin, setDestination } from "../slices/navSlice";
+import Navfavourites from "../components/Navfavourites";
+
+const HomeScreen = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
+        <GooglePlacesAutocomplete
+          placeholder="Where From?"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          fetchDetails={true}
+          enablePoweredByContainer={false}
+          returnKeyType={"search"}
+          minLength={2}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+
+            dispatch(setDestination(null));
+          }}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
+          }}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+        />
+
+        <NavOptions />
+        <Navfavourites />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
